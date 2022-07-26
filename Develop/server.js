085a093c1cc314fc8a5391908d9b4459c165a94b
MiloCOLO <100871599+MiloCOLO@users.noreply.github.com>
2022-07-25 22:10:49 -0600

    worked out the kinks and got the routes to work properly

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,20 @@
+const { json } = require('express');
 const express = require('express');
 const fs = require('fs');
 const path = require('path')
+const uuid = require('./public/assets/js/uuid.js')
 // adding express for streamlining 
 // adding fs to help with saving and retrieving notes
+// made the uuid file for unique note IDs
 
-const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get('/tutor', (req, res) =>{
-    res.send("hello")
-});
-
+const app = express(); //connecting express
+app.use(express.static('public')); //making the assets in the public folder static middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>{ //this will create a route to the notes page
-    console.log("I'm in the get route"),
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
@@ -20,9 +22,44 @@ app.get('/notes', (req, res) =>{ //this will create a route to the notes page
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.post('/notes', (req, res) => {
-  console.log("I'm in the post route"),
-  res.json(path.join(__dirname, '/public/notes.html'))
+app.get('/api/notes', (req, res) => { //this will read the db.json file and return notes as json content
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        }   else{res.json(JSON.parse(data))
+            console.log(data)
+        }
+    })
 });
 
-app.listen(3000);
+app.post("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        }   else {
+            const {title, text} = req.body
+            const message = {
+                title: title,
+                text: text,
+                id: uuid(),
+            }
+            const newNote = JSON.parse(data);
+            newNote.push(message);
+            console.log(newNote)
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(newNote, null, 4),
+                (err) =>
+                    err ? console.error(err) 
+                    : console.info('successfully logged request')
+            )
+        }
+    })
+});
+
+app.get('*', (req, res) =>{ //this will create a route to the notes page
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
+app.listen(3001);
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # note-taker
+
+What I've made is a note taking app that will allow people to write notes and save them. With a clean and simple layout, it makes it easy to create notes for work, home, etc. I created this using JavaScript and used express and fs to make it so a user can write notes, and using the GET/POST functions I was able to make it so a user could save their notes. 
+
+## Usage
+
+Using this is easy! Just open it up and start typing. This doesn't require rocket science. Yet. 
+
+## Technologies Used
+
+I wrote this using 
+
+* JavaScript
+* Express
+* FS
+* Node
+* NPM
+
+## Code Snippets
+
+I was able to create routes to separate pages using GET routes
+
+```
+app.get('/notes', (req, res) =>{ 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+```
+
+## Credits
+Shoutout to Kavya Mandla for helping me out with the POST function, and to my cat Mousse for giving me lap support.
+
+## License 
+
+This uses a standard MIT license. 
